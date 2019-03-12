eac49d4be1cb10d8c025f716f522ab2d85116306
mtrybus2208 <mtrybus2208@wp.pl>
2019-03-12 21:49:39 +0100

    Added CardButton component

---
+++ src/scenes/TimeLineBoard/components/MediaButton/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as S from './styles';
+
+const propTypes = {
+
+};
+
+const defaultProps = { 
+}; 
+
+const MediaButton = ({}) => {
+  return (
+    <S.MediaButton>
+
+    </S.MediaButton>
+  );
+};
+
+MediaButton.propTypes = propTypes;
+MediaButton.defaultProps = defaultProps;
+export default MediaButton;
---
+++ src/scenes/TimeLineBoard/components/UserInfoCard/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as S from './styles';
+
+const propTypes = {
+
+};
+
+const defaultProps = { 
+}; 
+
+const UserInfoCard = ({}) => {
+  return (
+    <S.UserInfoCard>
+
+    </S.UserInfoCard>
+  );
+};
+
+UserInfoCard.propTypes = propTypes;
+UserInfoCard.defaultProps = defaultProps;
+export default UserInfoCard;
