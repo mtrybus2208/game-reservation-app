fb193adbac5ed7c4f50619bc34b2fd81b21b55f2
stanislawczykj <jstanislawczyk@pgs-soft.com>
2019-04-04 16:24:13 +0200

    Prototype of global chat websocket connection

--- src/chat/components/GlobalChatWrapper/index.jsx
+++ src/chat/components/GlobalChatWrapper/index.jsx
@@ -1,125 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import * as S from './styles';
 
 const propTypes = {
   directChatIcon: PropTypes.string,
   emojiIcon: PropTypes.string,
   directChatIcon: PropTypes.string,
+  messages: PropTypes.array,
 };
 
 const defaultProps = {
   directChatIcon: 'https://res.cloudinary.com/dfmqgkkbx/image/upload/v1553587606/message-yellow.svg',
   emojiIcon: 'https://res.cloudinary.com/dfmqgkkbx/image/upload/v1553595074/smiling-emoticon.svg',
   sendMessageIcon: 'https://res.cloudinary.com/dfmqgkkbx/image/upload/v1553595060/send-button.svg',
+  messages: [],
 }; 
 
-const GlobalChatWrapper = ({ directChatIcon, emojiIcon, sendMessageIcon }) => {
+const GlobalChatWrapper = ({ directChatIcon, emojiIcon, sendMessageIcon, messages }) => {
+
+  useEffect(() => {
+    const websocket = new WebSocket('ws://localhost:9000/socket/chat/global');
+
+    websocket.onmessage = (event) => {
+      let message = JSON.parse(event.data);
+
+      axios.get(`http://localhost:9000/players/${message.playerId}`, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        })
+        .then(response => { 
+          message.playerName = response.data.displayName;
+          messages.push(message);
+        })
+    };
+  });
+
   return (
     <S.GlobalChatWrapper>
         <S.MessagesWrapper>
-          <S.Message>
-            <S.MessageHeader>
-              <S.PlayerName>
-                <S.PlayerNameText>
-                  Jakub Stanisławczyk
-                </S.PlayerNameText>
-              </S.PlayerName>
+          {messages.map((value, index) => (
+            <S.Message key={index}>
+              <S.MessageHeader>
+                <S.PlayerName>
+                  <S.PlayerNameText>    
+                    {value.playerName}
+                  </S.PlayerNameText>
+                </S.PlayerName>
 
-              <S.PlayerDirectChat>
-                <S.PlayerDirectChatIcon src={directChatIcon} />
-              </S.PlayerDirectChat>
-              
-              <S.PlayerPictureWrapper>
-                <S.PlayerPicture>
+                <S.PlayerDirectChat>
+                  <S.PlayerDirectChatIcon src={directChatIcon} />
+                </S.PlayerDirectChat>
                 
-                </S.PlayerPicture>
-              </S.PlayerPictureWrapper>
-            </S.MessageHeader>
-            
-            <S.MessageBody>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </S.MessageBody>
-          </S.Message>
-
-          <S.Message>
-            <S.MessageHeader>
-              <S.PlayerName>
-                <S.PlayerNameText>
-                  Michał Trybus
-                </S.PlayerNameText>
-              </S.PlayerName>
-
-              <S.PlayerDirectChat>
-                <S.PlayerDirectChatIcon src={directChatIcon} />
-              </S.PlayerDirectChat>
+                <S.PlayerPictureWrapper>
+                  <S.PlayerPicture>
+                  
+                  </S.PlayerPicture>
+                </S.PlayerPictureWrapper>
+              </S.MessageHeader>
               
-              <S.PlayerPictureWrapper>
-                <S.PlayerPicture>
-                
-                </S.PlayerPicture>
-              </S.PlayerPictureWrapper>
-            </S.MessageHeader>
-            
-            <S.MessageBody>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </S.MessageBody>
-          </S.Message>
-          <S.Message>
-            <S.MessageHeader>
-              <S.PlayerName>
-                <S.PlayerNameText>
-                  Jakub Stanisławczyk
-                </S.PlayerNameText>
-              </S.PlayerName>
-
-              <S.PlayerDirectChat>
-                <S.PlayerDirectChatIcon src={directChatIcon} />
-              </S.PlayerDirectChat>
-              
-              <S.PlayerPictureWrapper>
-                <S.PlayerPicture>
-                
-                </S.PlayerPicture>
-              </S.PlayerPictureWrapper>
-            </S.MessageHeader>
-            
-            <S.MessageBody>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </S.MessageBody>
-          </S.Message>
-
-          <S.Message>
-            <S.MessageHeader>
-              <S.PlayerName>
-                <S.PlayerNameText>
-                  Michał Trybus
-                </S.PlayerNameText>
-              </S.PlayerName>
-
-              <S.PlayerDirectChat>
-                <S.PlayerDirectChatIcon src={directChatIcon} />
-              </S.PlayerDirectChat>
-              
-              <S.PlayerPictureWrapper>
-                <S.PlayerPicture>
-                
-                </S.PlayerPicture>
-              </S.PlayerPictureWrapper>
-            </S.MessageHeader>
-            
-            <S.MessageBody>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </S.MessageBody>
-          </S.Message>
+              <S.MessageBody>
+                {value.message}
+              </S.MessageBody>
+            </S.Message>
+          ))}
         </S.MessagesWrapper>
 
         <S.MessageInputWrapper>
