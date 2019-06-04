4bf59cf547ea5d70b79be182fffb931a08073837
PGS-SOFT\mtrybus <mtrybus@pgs-soft.com>
2019-06-04 16:13:01 +0200

    Created final version of mocked card movement

--- src/modules/home/components/MockGameCard/index.jsx
+++ src/modules/home/components/MockGameCard/index.jsx
@@ -1,18 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react'; 
+import React from 'react'; 
 import PropTypes from 'prop-types';
 import GameCard from '@/modules/home/components/GameCard';
 import BaseIcon from '@/modules/shared/components/BaseIcon';
 import * as S from './styles';
-
-/**
- * Create handlers foe every directions
- * 1. left
- * 2. right
- * 3. right interval 
- * 4. left interval 
- * 5. right interval + change direction to left
- * 6. left interval + change direction to right
- */
 
 const propTypes = {
   authUser: PropTypes.object,
@@ -34,50 +24,39 @@ class MockGameCard extends React.PureComponent {
     translateX: 0,
     lastTranslateX: 0, 
     able: false,
-    test: 0,
-    intervalMove: false,
-    isRightEdge: false,
+    intervalDirection: null,
   };
-  handleNormalMove = this.handleNormalMove.bind(this);
-  leftMoveSimple = this.leftMoveSimple.bind(this);
+   
+  resetInterval = this.resetInterval.bind(this);
   animLoop = this.animLoop.bind(this);
   myRef = React.createRef();
-  running = true;
-
-  localMouseDirection = {
-    x: 0,
-  }
+  running = true; 
 
   componentDidMount() {
-    this.animLoop( ( deltaT ) => {
-      if(this.state.able) {
-        this.tick(); 
+    this.animLoop(() => {
+      if (this.state.able) {
+        this.tick();
       }
-  }, this.myRef.current );
-}
-
-  componentDidUpdate(prevProps, prevState) { 
+    }, this.myRef.current );
   }
+
   componentWillUnmount() {
     window.removeEventListener('mousemove', this.handleMouseMove);
     window.removeEventListener('mouseup', this.handleMouseUp);
   }
 
-  animLoop( render, element ) {
-    let lastFrame = +new Date; 
-    const loop = ( now ) => { 
-  
-      if ( this.running !== false) {
-          requestAnimationFrame( loop, element );
-            if(this.state.able !== false) {
-              this.running = render( now - lastFrame );
-              lastFrame = now;
-
-            } 
+  animLoop(render, element) {
+    let lastFrame = +new Date();
+    const loop = now => {
+      if (this.running !== false) {
+        requestAnimationFrame(loop, element);
+        if (this.state.able !== false) {
+          this.running = render( now - lastFrame );
+          lastFrame = now;
+        }
       }
-    }
-
-    loop( lastFrame ); 
+    };
+    loop(lastFrame);
   }
 
   customTitle = (
@@ -88,144 +67,121 @@ class MockGameCard extends React.PureComponent {
       />
     </S.AnimatedIcon>
   )
-  
+
   handleMouseDown = ({ clientX, clientY }) => {
     window.addEventListener('mousemove', this.handleMouseMove);
     window.addEventListener('mouseup', this.handleMouseUp);
 
-    this.setState( prev =>
+    this.setState(prev =>
       ({ 
-        lastTranslateX: !prev.able ? parseInt(this.myRef.current.style.left) : this.state.translateX,
-        originalX: clientX, 
+        lastTranslateX: parseInt(this.myRef.current.style.left, 10),
+        originalX: clientX,
         isDragging: true,
         isAbleToMove: true,
         able: false,
       }),
-      () => { 
-
-        this.props.onBlockTimeLine(true);
-      },
+    () => {
+      this.props.onBlockTimeLine(true);
+    },
     );
- 
-  };
+  }; 
 
-  handleNormalMove(clientX) {
+  resetInterval(clientX) {
+    this.setState({
+      originalX: clientX, 
+      lastTranslateX: parseInt(this.myRef.current.style.left),
+      isDragging: true,
+      isAbleToMove: true,
+      able: false,
+      intervalDirection: null,
+    });
+  }
+
+  detectIntervalReset = (directionX) => {
+    return (directionX > 0 && this.state.intervalDirection === 'left') || (directionX < 0 && this.state.intervalDirection === 'right');
+  }
+
+  isLeftEdge = () => {
+    return (this.myRef.current.offsetParent.scrollLeft > this.state.translateX);
+  }
+
+  handleMouseMove = e => {
+    const { clientX } = e;
     const { isDragging } = this.state;
     const { current } = this.myRef;
     const { offsetParent } = current;
 
-    if (!isDragging) {
-      return;
-    }
-    
-    const isRightEdge = this.state.translateX + this.props.display.size >= offsetParent.offsetWidth + offsetParent.scrollLeft; 
-    this.setState(prevState => ({  
-      isRightEdge,
-      translateX: isRightEdge ? offsetParent.offsetWidth + offsetParent.scrollLeft - this.props.display.size : clientX - prevState.originalX + prevState.lastTranslateX,
-      
-    }), () => {
-      if(this.state.isRightEdge) {
-        this.setState(prevState => ({
-          able: true,
-        }), () => { 
-        });
-      }
-    }); 
-  }
-
-  leftMoveSimple(clientX) { 
-    this.setState( prev =>
-      ({
-        originalX: clientX, 
-        lastTranslateX: parseInt(this.myRef.current.style.left),
-        isDragging: true,
-        isAbleToMove: true,
-        able: false,
-        isRightEdge: false
-      }),
-    );
-  }
-
-  handleMouseMove = (e) => {
-    const {clientX } = e;
-    const { isDragging } = this.state;
-
     const directionX = e.movementX || e.mozMovementX || e.webkitMovementX || 0;
-    const isRightEdge = this.state.translateX + this.props.display.size >= this.myRef.current.offsetParent.offsetWidth +this.myRef.current. offsetParent.scrollLeft;  
-
-    const mouseLeftMove = clientX - this.state.originalX < 0;
-    const mouseRightMove = clientX - this.state.originalX > 0;
+    const isRightEdge = parseInt(current.style.left, 10) + this.props.display.size >= current.offsetParent.offsetWidth + offsetParent.scrollLeft;
 
     if (!isDragging) {
       return;
     }
 
-    if(directionX <0 && isRightEdge) {
-      console.log('end of wrapper');
-
-      this.setState(prevState => ({  
-        isRightEdge: false,
-        translateX: clientX - prevState.originalX + prevState.lastTranslateX,
-        able: false,
-        isAbleToMove: true,
-      }));
-      return false;
-    }
-
-
-    if(mouseRightMove && directionX <0 && !this.state.able && !this.state.isRightEdge) {
-      this.leftMoveSimple(clientX)
+    if (this.detectIntervalReset(directionX)) {
+      this.resetInterval(clientX);
       return;
     }
 
-    if(mouseRightMove && this.state.able && directionX < 0) { 
+    if (this.isLeftEdge() && directionX < 0) {
       this.setState({
-        able: false,
-        isRightEdge: false,
-      })
+        able: true,
+        intervalDirection: 'left',
+      });
       return;
-    } 
+    }
 
-     this.handleNormalMove(clientX);
+    if (!this.state.able) {
+      this.setState(prevState => ({
+        translateX: clientX - prevState.originalX + prevState.lastTranslateX,
+        intervalDirection: null,
+        able: false,
+      }));
+    }
+
+    if (isRightEdge && directionX > 0 && !this.state.able) {
+      this.setState(prevState => ({
+        able: true,
+        intervalDirection: 'right',
+        translateX: isRightEdge ? offsetParent.offsetWidth + offsetParent.scrollLeft - this.props.display.size : clientX - prevState.originalX + prevState.lastTranslateX,
+      }));
+    }
   };
 
   handleMouseUp = () => {
     window.removeEventListener('mousemove', this.handleMouseMove);
     window.removeEventListener('mouseup', this.handleMouseUp);
     
-    this.setState( prev =>
-      ({
-        originalX: 0,
-        originalY: 0,
-        lastTranslateX: prev.able ? parseInt(this.myRef.current.style.left) : this.state.translateX,
+    this.setState(prev => ({
+      originalX: 0,
+      originalY: 0,
+      lastTranslateX: prev.able ? parseInt(this.myRef.current.style.left, 10) : this.state.translateX,
 
-        isDragging: false,
-        isAbleToMove: false, 
-        able: false,
-      }),
-      () => { 
-
-        this.props.onBlockTimeLine(false);
-      },
-    );
-  };  
-
-  isRightEdge = () => {
-    const { current } = this.myRef;
-    const { offsetParent } = current;
-    const { display } = this.props; 
-
-    return current.offsetLeft + display.size >= offsetParent.offsetWidth + offsetParent.scrollLeft;
-  }
+      isDragging: false,
+      isAbleToMove: false, 
+      able: false,
+    }), () => {
+      this.props.onBlockTimeLine(false);
+    });
+  };
 
   tick = () => {
     const { current } = this.myRef;
-    this.props.onMoveTimeLine(+13);
-    const computed = (this.myRef.current.offsetParent.offsetWidth + this.myRef.current.offsetParent.scrollLeft) - this.props.display.size;
-    this.myRef.current.style.left = `${computed}px`;
+
+    if (this.state.intervalDirection === 'left' && this.state.able) {
+      this.props.onMoveTimeLine(-13);
+      const computed = current.offsetParent.scrollLeft;
+      current.style.left = `${computed}px`;
+      return;
+    }
+
+    if (this.state.intervalDirection === 'right' && this.state.able) {
+      this.props.onMoveTimeLine(13);
+      const computed = (current.offsetParent.offsetWidth + current.offsetParent.scrollLeft) - this.props.display.size;
+      current.style.left = `${computed}px`;
+    }
   }
 
- 
   render() {
     const { translateX, translateY, isDragging } = this.state;
     return (
