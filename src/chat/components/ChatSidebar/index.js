a4cfffb19dd123b4490bb750e439ac8b2b0c8a67
xenonso <jakubstanislawczyk98@gmail.com>
2019-06-05 14:01:59 +0200

    New chat section wrapper

---
+++ src/chat/components/ChatSidebar/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import GlobalChatWrapper from '../GlobalChatWrapper/index';
+import DirectChatWrapper from '../DirectChatWrapper/index';
+import { connect } from 'react-redux';
+import * as fromActions from '../../state/actions';
+import * as S from './styles';
+
+const propTypes = { 
+    chat: PropTypes.object.isRequired,
+    authUser: PropTypes.object,
+};
+
+const defaultProps = { };
+
+class ChatSidebar extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.setGlobalChatWebsocketConnection = this.setGlobalChatWebsocketConnection.bind(this)
+    }
+
+    state = {
+        globalChatWebsocket: null,
+    };
+
+    setGlobalChatWebsocketConnection(websocket) {
+        this.setState({
+            globalChatWebsocket: websocket,
+        });
+    }
+
+    render() {
+        return ( 
+            <React.Fragment>
+                {this.props.chat.chatMode !== 'GLOBAL' ? (
+                    <DirectChatWrapper 
+                        authUser={this.props.authUser} 
+                        receiverId={this.props.chat.chatMode}
+                    />
+                    ) : (
+                    <GlobalChatWrapper 
+                        authUser={this.props.authUser} 
+                        globalChatMessages={this.props.chat.globalChatMessages}
+                        globalChatWebsocket={this.state.globalChatWebsocket}
+                        setGlobalChatWebsocketConnection={this.setGlobalChatWebsocketConnection}
+                    />
+                )}
+            </React.Fragment>
+        )
+    }
+}
+
+ChatSidebar.propTypes = propTypes;
+ChatSidebar.defaultProps = defaultProps;
+export default ChatSidebar;
+
