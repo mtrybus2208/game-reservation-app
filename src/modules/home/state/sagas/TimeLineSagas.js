afc6be3d510c1fd4849311e441143a8065fc76cd
PGS-SOFT\mtrybus <mtrybus@pgs-soft.com>
2019-06-10 12:35:04 +0200

    Filtered old games

--- src/modules/home/state/sagas/TimeLineSagas.js
+++ src/modules/home/state/sagas/TimeLineSagas.js
@@ -1,6 +1,7 @@
 /* eslint no-use-before-define: 0 */
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery, select } from 'redux-saga/effects';
 import { actionTypes } from './../actions/actionTypes';
+import moment from 'moment';
 
 const makeEntieties = arr => {
   const ent = arr.reduce((obj, item) => ({
@@ -17,21 +18,24 @@ const makeEntieties = arr => {
   return ent;
 };
 
-const fetchGames = () => (
+const fetchGames = (workdayStart) => (
   fetch(`http://3.95.208.60/matches`)
-    .then(response => response.json())
+    .then(res => res.json())
+    .then((res) => res.filter(game =>
+      moment(game.startDate).isAfter(moment(workdayStart))))
     .then(makeEntieties)
 );
 
 const fetchPlayers = () => (
   fetch(`http://3.95.208.60/players`)
-    .then(response => response.json())
+    .then(res => res.json())
     .then(makeEntieties)
 );
 
 function* workFetchReservedGames() {
   try {
-    const games = yield call(fetchGames);
+    const { workdayStart } = yield select(state => state.timeLine);
+    const games = yield call(fetchGames, workdayStart);
     const players = yield call(fetchPlayers);
     yield put({ type: actionTypes.FETCH_PLAYERS, players });
     yield put({ type: actionTypes.FETCH_RESERVED_GAMES_SUCCESS, games });
