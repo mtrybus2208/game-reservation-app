13363e1b497d1180389831189f62c00583ea9864
PGS-SOFT\mtrybus <mtrybus@pgs-soft.com>
2019-07-15 15:54:23 +0200

    Updated redux

--- src/modules/shared/containers/Modals/AddGameModal/index.js
+++ src/modules/shared/containers/Modals/AddGameModal/index.js
@@ -1,58 +1,34 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import BaseModal from '@/modules/shared/components/Modals/BaseModal';
 import * as fromActions from '@/modules/shared/state/actions';
 import * as fromHomeActions from '@/modules/home/state/actions';
 
-const propTypes = {
-  addGame: PropTypes.func,
-  hideModal: PropTypes.func,
-  modalProps: PropTypes.any,
-  isAddGameFetching: PropTypes.bool,
-};
-const defaultProps = {
-  isAddGameFetching: false,
-};
-class AddGameModal extends PureComponent {
-  handlerAddGame = this.handlerAddGame.bind(this);
+const propTypes = {};
+const defaultProps = {};
 
-  handlerAddGame() {
-    const { addGame, modalProps } = this.props;
-    addGame(modalProps.gameId);
-  }
+const AddGameModal = () => {
+  const modalProps = useSelector(({ modal }) => modal.modalProps);
 
-  render() {
-    return (
-      <BaseModal
-        title="Do You want to reserve game at: 13:00?"
-        onConfirm={this.handlerAddGame}
-        onDecline={this.props.hideModal}
-        isLoading={this.props.isAddGameFetching}
-      />
-    );
-  }
-}
+  const isAddGameFetching = useSelector(({ timeLine }) => timeLine.isAddGameFetching);
 
-const mapStateToProps = ({ modal, timeLine }) => (
-  {
-    modalProps: modal.modalProps,
-    isAddGameFetching: timeLine.isAddGameFetching,
-  }
-);
+  const dispatch = useDispatch();
 
-const mapDispatchToProps = dispatch => {
-  return {
-    hideModal: () => {
-      dispatch(fromActions.hideModal());
-    },
+  const handlerHideModal = () => dispatch(fromActions.hideModal());
 
-    addGame: payload => {
-      dispatch(fromHomeActions.addNewGame(payload));
-    },
-  };
+  const handlerAddGame = () => dispatch(fromHomeActions.addNewGame(modalProps.gameId));
+
+  return (
+    <BaseModal
+      title="Do You want to reserve game at: 13:00?"
+      onConfirm={handlerAddGame}
+      onDecline={handlerHideModal}
+      isLoading={isAddGameFetching}
+    />
+  );
 };
 
 AddGameModal.propTypes = propTypes;
 AddGameModal.defaultProps = defaultProps;
-export default connect(mapStateToProps, mapDispatchToProps)(AddGameModal);
+export default AddGameModal;
