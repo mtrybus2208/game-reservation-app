f697904344809a34f8e20d9aab88942e4d748318
xenonso <jakubstanislawczyk98@gmail.com>
2019-09-22 22:02:55 +0200

    Updating state about active players component

--- src/modules/chat/state/reducers/index.js
+++ src/modules/chat/state/reducers/index.js
@@ -13,6 +13,11 @@ export const chatReducer = (state = initialState, action) => {
         ...state,
         chatMode: 'GLOBAL',
       };
+    case actionTypes.SET_ACTIVE_PLAYERS_MODE:
+      return {
+        ...state,
+        chatMode: 'ACTIVE_PLAYERS',
+      };
     case actionTypes.SET_DIRECT_CHAT_MODE:
       return {
         ...state,
