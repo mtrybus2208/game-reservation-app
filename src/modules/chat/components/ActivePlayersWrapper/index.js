f0b0e1b23ac8f719ee30613884c9d0f9ac16fb7f
xenonso <jakubstanislawczyk98@gmail.com>
2019-09-22 22:02:37 +0200

    New active players component

---
+++ src/modules/chat/components/ActivePlayersWrapper/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as fromActions from '../../state/actions';
+import * as S from './styles';
+
+const propTypes = { 
+    setGlobalChatMode: PropTypes.func,
+    isActivePlayersListMode: PropTypes.bool,
+};
+
+const defaultProps = { };
+
+class ActivePlayersWrapper extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+
+    }
+
+    links = {
+        globalChatIcon: 'https://res.cloudinary.com/dfmqgkkbx/image/upload/v1553015547/message.svg',
+    }
+
+    openGlobalChat = () => {
+        this.props.setGlobalChatMode();
+    }
+
+    render() {
+        return ( 
+            <S.ActivePlayersWrapper
+                isActivePlayersListMode={this.props.isActivePlayersListMode}
+            >
+                <S.GlobalChatReturn onClick={this.openGlobalChat}>
+                    <S.GlobalChatIcon src={this.links.globalChatIcon} />
+                    <S.GlobalChatInfo>Global chat</S.GlobalChatInfo>
+                </S.GlobalChatReturn>
+
+            </S.ActivePlayersWrapper>
+        )
+    }
+}
+
+const mapStateToProps = () => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setGlobalChatMode: () => {
+            dispatch(fromActions.setGlobalChatMode());
+        },
+    };
+};
+  
+ActivePlayersWrapper.propTypes = propTypes;
+ActivePlayersWrapper.defaultProps = defaultProps;
+export default connect(mapStateToProps, mapDispatchToProps)(ActivePlayersWrapper);
---
+++ src/modules/chat/components/ActivePlayersWrapper/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const ActivePlayersWrapper = styled.div`
+  height: 100%;
+  display: ${props => (props.isActivePlayersListMode ? 'flex' : 'none')};
+  flex-direction: column;
+  align-items: center;
+  background: #1b1e22;
+`;
+
+export const GlobalChatReturn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  border: none;
+  background: #111315;
+  cursor: pointer;
+`;
+
+export const GlobalChatIcon = styled.img`
+  width: 16px;
+`;
+
+export const GlobalChatInfo = styled.span`
+  margin-left: 12px;
+  font-size: 10px;
+  font-weight: 300;
+  line-height: 1;
+  letter-spacing: .3px;
+  text-align: left;
+  text-transform: uppercase;
+  color: #858b92;
+`;
